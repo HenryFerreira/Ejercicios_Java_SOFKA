775de2a704373e663f9cf2346900066ba73abaef
HenryFerreira <henryferreira133@gmail.com>
2022-02-11 12:09:24 -0300

    Ejercicio 1y2 - Comparar Valores y Ejercicio 3 - Area del Circulo

---
+++ src/calcularAreas.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class calcularAreas {
+    static Scanner scanner = new Scanner(System.in);
+    public static void areaDelCirculo(int radio){
+       double resultado;
+       resultado = Math.PI * (Math.pow(radio,2));
+
+        System.out.println("El area del Circulo con radio " + radio +" es de: [" + resultado +"]");
+    }
+
+    public static void main(String[] args) {
+        int radio;
+        System.out.println("|Bienvenido al Programa|\n");
+        System.out.println("Ingrese el Radio del circulo: ");
+        radio = scanner.nextInt();
+
+        areaDelCirculo(radio);
+
+    }
+}
---
+++ src/comprobarValores.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class comprobarValores {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void compararValores(int num1, int num2){
+        if(num1 == num2){//Si los numeros so iguales
+            System.out.println("Los números ingresados son iguales: [" + num1 + "] = [" + num2+ "]");
+        } else if (num1 > num2){//Si el numero 1 es mayor
+            System.out.println("El número "+ num1 +" es mayor que el número "+ num2 +": [" + num1 + "] > [" + num2+ "]");
+        } else {//Si el numero 1 es menor
+            System.out.println("El número "+ num1 +" es menor que el número "+ num2 +": [" + num1 + "] < [" + num2+ "]");
+        }
+    }
+
+    public static void main(String[] args) {
+        int num1, num2;
+        System.out.println("|Bienvenido al Programa|\n");
+        System.out.println("Ingrese el primero número: ");
+        num1 = scanner.nextInt();
+        System.out.println("Ingrese el segundo número: ");
+        num2 = scanner.nextInt();
+
+        compararValores(num1,num2);
+    }
+}
