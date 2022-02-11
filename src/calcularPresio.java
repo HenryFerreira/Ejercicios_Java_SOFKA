a5e681990e7e8a2bc71914666bcdf7efa5383933
HenryFerreira <henryferreira133@gmail.com>
2022-02-11 14:42:28 -0300

    Ejercicio 4 - Calcular precio con IVA

---
+++ src/calcularPresio.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class calcularPresio {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void presioConIVA(float precio){
+        float iva = 0.21F;
+        float total = precio + (precio * iva);
+
+        System.out.println("El precio del producto es de $" + total);
+    }
+
+    public static void main(String[] args) {
+        float precio;
+        System.out.println("|Bienvenido al Programa - Calcular Presio con IVA|\n");
+        System.out.print("Ingrese el precio del producto: ");
+        precio = scanner.nextFloat();
+        presioConIVA(precio);
+    }
+}
