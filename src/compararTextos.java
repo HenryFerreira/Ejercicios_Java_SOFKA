ec47ceaed0f548296b50f8afdcab53f645ce39a3
HenryFerreira <henryferreira133@gmail.com>
2022-02-11 22:05:20 -0300

    Ejercicio 12 - Comparar Palabras

---
+++ src/compararTextos.java
@@ -0,0 +1,28 @@
+import com.sun.tools.javac.Main;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class compararTextos {
+    static Scanner scanner = new Scanner(System.in);
+    public static void verificarIgualdadDeStrings(String tex_1, String tex_2) {
+        if (tex_1.equals(tex_2)) {
+            System.out.println("Las palabras son IGUALES: [" + tex_1 + "]");
+        } else {
+            System.out.println("Las palabras son DIFERENTES: [" + tex_1 + "] != [" + tex_2 + "]");
+        }
+    }
+
+    public static void main(String[] args) {
+        String tex_1, tex_2;
+        System.out.println("|Bienvenido al Programa - Comparar Palabras|");
+        System.out.print("Digite la primera palabra: ");
+        tex_1 = scanner.nextLine();
+
+        System.out.print("\nDigite la segunda palabra: ");
+        tex_2 = scanner.nextLine();
+
+        verificarIgualdadDeStrings(tex_1, tex_2);
+    }
+}
