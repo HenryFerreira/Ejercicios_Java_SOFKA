8340c7c73828a65b14ad072138bf691536b39e9b
HenryFerreira <henryferreira133@gmail.com>
2022-02-11 22:28:01 -0300

    Ejercicio 13 - Hora y fecha actual

---
+++ src/consultarFecha.java
@@ -0,0 +1,17 @@
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.zip.DataFormatException;
+
+public class consultarFecha {
+    public static void consultarFechaHoraActual(){
+        LocalDateTime fechaHoraActual = LocalDateTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("(yyyy/MM/dd) (HH:mm:ss)");
+
+        System.out.println(fechaHoraActual.format(formato));
+    }
+
+    public static void main(String[] args) {
+        consultarFechaHoraActual();
+    }
+}
